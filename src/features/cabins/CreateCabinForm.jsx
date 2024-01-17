db9759a9deb3fc8e35227e3dbd05a62a597b6e13
ShawnRSidwell <shawnrsidwell@gmail.com>
2024-01-17 11:21:40 -0800

    Add image uploading

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -63,7 +63,7 @@ function CreateCabinForm() {
   });
 
   function onSubmit(data) {
-    mutate(data);
+    mutate({ ...data, image: data.image[0] });
   }
 
   function onError(errors) {
@@ -133,74 +133,12 @@ function CreateCabinForm() {
       </FormRow>
 
       <FormRow label="Cabin photo">
-        <FileInput id="image" accept="image/*" />
-      </FormRow>
-
-      {/*     
-      <FormRow>
-        <Label htmlFor="name">Cabin name</Label>
-        <Input
-          type="text"
-          id="name"
-          {...register("name", { required: "This field is required" })}
-        />
-        {errors?.name?.message && <Error>{errors.name.message}</Error>}
-      </FormRow>
-
-      <FormRow>
-        <Label htmlFor="maxCapacity">Maximum capacity</Label>
-        <Input
-          type="number"
-          id="maxCapacity"
-          {...register("maxCapacity", {
-            required: "This field is required",
-            min: { value: 1, message: "Capacity should be at least 1" },
-          })}
+        <FileInput
+          id="image"
+          accept="image/*"
+          {...register("image", { required: "This field is required" })}
         />
       </FormRow>
-
-      <FormRow>
-        <Label htmlFor="regularPrice">Regular price</Label>
-        <Input
-          type="number"
-          id="regularPrice"
-          {...register("regularPrice", {
-            required: "This field is required",
-            min: { value: 1, message: "Price should be greater than a dollar" },
-          })}
-        />
-      </FormRow>
-
-      <FormRow>
-        <Label htmlFor="discount">Discount</Label>
-        <Input
-          type="number"
-          id="discount"
-          defaultValue={0}
-          {...register("discount", {
-            required: "This field is required",
-            validate: (value) =>
-              value <= getValues().regularPrice ||
-              "Discount should be less than regular price",
-          })}
-        />
-      </FormRow>
-
-      <FormRow>
-        <Label htmlFor="description">Description for website</Label>
-        <Textarea
-          type="number"
-          id="description"
-          defaultValue=""
-          {...register("description", { required: "This field is required" })}
-        />
-      </FormRow>
-
-      <FormRow>
-        <Label htmlFor="image">Cabin photo</Label>
-        <FileInput id="image" accept="image/*" />
-      {/* </FormRow> */}
-
       <FormRow>
         <Button variation="secondary" type="reset">
           Cancel
--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://depfbcqhhmlfcmqjzsdi.supabase.co";
+export const supabaseUrl = "https://depfbcqhhmlfcmqjzsdi.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRlcGZiY3FoaG1sZmNtcWp6c2RpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQ1ODY2MDUsImV4cCI6MjAyMDE2MjYwNX0.TpKk7pX0cW4crz3kTxv_1z-OLbecWXEeg4RqoWaJ5F4";
 const supabase = createClient(supabaseUrl, supabaseKey);
